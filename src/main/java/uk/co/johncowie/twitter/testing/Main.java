1db4cc03ca8a25f0b2f67387577936f461dbd812
John Cowie <john.a.cowie@gmail.com>
2013-07-24 13:24:38 +0100

    Got following/unfollowing working

--- src/main/java/uk/co/johncowie/twitter/testing/Main.java
+++ src/main/java/uk/co/johncowie/twitter/testing/Main.java
@@ -1,5 +1,8 @@
 package uk.co.johncowie.twitter.testing;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.PropertyConfiguration;
@@ -9,13 +12,16 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, TwitterException {
         PropertyConfiguration propertyConfiguration
                 = new PropertyConfiguration(new FileInputStream("/Users/jcowie/Dropbox/nuotltester.properties"));
         TwitterStream stream = new TwitterStreamFactory(propertyConfiguration).getInstance();
+        final Twitter twitter = new TwitterFactory(propertyConfiguration).getInstance();
+//        twitter.createFriendship("nuotltestuser");
+        twitter.destroyFriendship("nuotltestuser");
         stream.addListener(new CustomUserStreamListener());
         stream.addListener(new CustomRawStreamListener());
-        stream.user(new String[]{"@nextuptester"});
+        stream.user();
     }
 
 
